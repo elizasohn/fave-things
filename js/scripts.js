9648a6d8259d6d1b62a606084b2da56f4a7bb658
Student <example@example.com>
2019-10-28 10:41:32 -0700

    push array objects into new array, display as list items in ul

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,27 @@ $(document).ready(function(){
     var music = $("#musicInput").val();
     var plant = $("#plantInput").val();
     var music2 = $("#musicInput2").val();
+
     var inputArray = [color, food, music, plant];
-    console.log(inputArray)
     var newArray = inputArray.slice()
+
     newArray.splice(2, 1, music2);
-    console.log(newArray)
+    var anotherArray = [];
+    anotherArray.push(inputArray[1]);
+    anotherArray.push(inputArray[0]);
+    anotherArray.push(inputArray[2]);
+
     var output = $(".result").text(inputArray);
     var output2 = $(".result2").text(newArray);
+    var output3 = $(".result3").text(anotherArray);
+    var list1 = $(".li1").text(anotherArray[0]);
+    var list2 = $(".li2").text(anotherArray[1]);
+    var list3 = $(".li3").text(anotherArray[2]);
     $(".end").show();
 
+
+
+    console.log(anotherArray)
   })
+
 })
